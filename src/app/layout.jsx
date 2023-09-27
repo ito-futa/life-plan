141d7b59cc2a8c128645fcd5ec14154667b86216
if <trident.ito@gmail.com>
2023-09-28 03:49:32 +0900

    フッターとヘッダーを追加、ローカルスコープでCSS追加

---
+++ src/app/layout.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Inter } from 'next/font/google'
+import './globals.css'
+import Header from './header/page'
+import Footer from './footer/page'
+import Head from 'next/head'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function RootLayout({ children }) {
+
+  return (
+    <html lang="ja">
+      <body className={inter.className}>
+        <Header />
+        <main>
+          {children}
+        </main>
+        <Footer />
+      </body>
+    </html >
+  )
+}
